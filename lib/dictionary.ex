a349773efe32c967f939ea8f6005021ca236d7fd
Don Lettrich <don.lettrich@gmail.com>
2018-04-29 21:40:56 -0400

    Made Dictionary a Free Standing Application

--- lib/dictionary.ex
+++ lib/dictionary.ex
@@ -1,14 +1,5 @@
 defmodule Dictionary do
+  alias Dictionary.WordList
 
-  def random_word() do
-    word_list()
-    |> Enum.random()
-  end
-
-  def word_list do
-    "../assets/words.txt"
-    |> Path.expand(__DIR__)
-    |> File.read!()
-    |> String.split(~r/\n/)
-  end
+  defdelegate random_word(), to: WordList
 end
